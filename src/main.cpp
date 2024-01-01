585a9701411d2033f2ca7af89d4970e0ef748b11
GZH <zhhge@sse.com.cn>
2024-01-01 22:05:08 +0800

    修正cmakelists写法

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+
+struct SALEDATA
+{
+    int sold_num = 0;
+};
+
 int main()
 {
+    SALEDATA sale_data ;
+    sale_data.sold_num = 10;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
